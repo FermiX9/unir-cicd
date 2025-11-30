cbc175d5eb305335a93e2e72bc13c7e2745fae28
Fermín Robledillo <Fermn Robledillo>
2025-11-30 22:03:49 +0100

    Added jenkins console output file

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -1,26 +1,53 @@
 pipeline {
     agent {
-        label 'docker'
+        label 'pc'
     }
     stages {
+        stage('Source') {
+            steps {
+                git 'https://github.com/FermiX9/unir-test.git'
+                }
+            }        
         stage('Build') {
             steps {
                 echo 'Building stage!'
                 sh 'make build'
                 }
             }
-        }
         stage('Unit tests') {
             steps {
                 sh 'make test-unit'
                 archiveArtifacts artifacts: 'results/*.xml'                
                 }
             }
+        stage('API tests') {
+            steps {
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+        stage('E2E tests') {
+            steps {
+                sh 'make test-e2e'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
         }
     }
     post {
         always {
             junit 'results/*_result.xml'
+        }
+        success {
+            echo "Pipeline completed successfully! Pipeline: ${env.JOB_NAME} Build Number: ${env.BUILD_NUMBER} URL de build: ${env.BUILD_URL}"
+        }
+        failure {
+            echo 'Pipeline failed. Please check the logs.'
+            mail body: "<br>Pipeline: ${env.JOB_NAME} <br>Build Number: ${env.BUILD_NUMBER} <br> URL de build: ${env.BUILD_URL}", 
+            charset: 'UTF-8', 
+            from: 'jenkins@domain.com',
+            mimeType: 'text/html', 
+            subject: "ERROR CI: Pipeline name -> ${env.JOB_NAME}", 
+            to: "fermin.robledillo122@comunidadunir.net";
         }
     }
 }
